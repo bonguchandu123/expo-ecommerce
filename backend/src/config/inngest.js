2b3e571d2fc5473e5f392989f3ea1d4457b5bc00
dineshkasi747 <dineshkasi000@gmail.com>
2025-12-04 16:20:42 +0530

    email

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,45 +1,38 @@
-import {Inngest} from "inngest"
-import { connectDB } from "./db.js"
-import {User} from "../models/user.model.js"
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
 
-export const inngest = new Inngest({id:"expo-ecommerce"})
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
 const syncUser = inngest.createFunction(
-    {id:"cler-user-id"},
-    {event:"clerk/user.created"},
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-    async ({event})=>{
-        await connectDB()
-        const {email_address,first_name,last_name,image_url,id} = event.data
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishlist: [],
+    };
 
-
-        const newUser = {
-            clerkId:id,
-            email:email_address,
-            name:`${first_name || ""} ${last_name || ""}`,
-            imageUrl:image_url,
-            addresses:[],
-            wishlist:[]
-        }
-
-        await User.create(newUser)
-    }
+    await User.create(newUser);
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id:"cler-user"},
-    {event:"clerk/user.deleted"},
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
-    async({event})=>{
-        await connectDB()
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  }
+);
 
-        const {id} = event.data
-
-        await User.deleteOne({clerkId:id})
-    }
-)
-
-
-
-
-export const functions = [syncUser,deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB];
